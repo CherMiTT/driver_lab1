4f2a4de85b770e9c009f875c5dab620bc617ac83
CherMi <chermi20000@yandex.ru>
2021-10-23 17:01:55 +0300

    Added params

--- my_hello.c
+++ my_hello.c
@@ -2,15 +2,31 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+static char *whom = "MAI";
+module_param(whom, charp, 0644);
+MODULE_PARM_DESC(whom, "The one we say hello to");
+
+static char *from = "425";
+module_param(from, charp, 0644);
+MODULE_PARM_DESC(from, "The one who says hello");
+
+static int number = 1;
+module_param(number, int, 0644);
+MODULE_PARM_DESC(number, "Number of greetings");
+
 static int __init hello_init(void)
 {
-	pr_alert("Hello, MAI from 425\n");
+	int i;
+	for(i = 0; i < number; ++i) 
+	{
+		pr_alert("Hello, %s from %s (greeting %d)\n", whom, from, i);
+	}
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	pr_alert("Good bay MAI\n");
+	pr_alert("Good bay, %s\n", whom);
 }
 
 module_init(hello_init);
